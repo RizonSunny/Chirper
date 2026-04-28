97301f12d4b5ba1ab7e6c2b90ec1405c182ac0f0
RizonSunny <rizon.sunny@gmail.com>
2026-04-28 10:41:06 +0600

    ORM

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChirpController::class, 'index']);
